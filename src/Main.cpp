c14ea3e7686acf96d88842792c86ad92c6b021fc
Hassan Pouralijan <pouralijan@gmail.com>
2019-05-21 17:31:25 +0430

    Updates CMakeLists.txt and build script for support clang compiler.

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,11 @@
 
 int main()
 {
+	static_assert(3 <= sizeof(int), "int are too small");
+	
 	MyCode::Vector v(10);
-	std::cout << v[-1] << std::endl;
+	std::cout << v[1] << std::endl;
+	v.size();
+	
 	return EXIT_SUCCESS;
 }
